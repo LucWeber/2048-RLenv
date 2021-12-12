1fa6e23325c1ebbf2f5049478aaf8ecacf3e373d
lucas <lucasweber000@gmail.com>
2021-12-12 17:18:21 +0100

    Cosmetics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import argparse
-sys.path.insert(0, '/home/lucas/Desktop/2048-python')
+sys.path.insert(0, '/home/lucas/Desktop/2048-RL-env')
 import matplotlib.pyplot as plt
 from env_2048 import Env_2048
 from env_sanity_check import EnvDummy
@@ -17,37 +17,39 @@ import logging
 #       -> if it cannot learn it -> check the grads
 # TODO: adapt script such that it is runable on HPC
 
-# GET ARGS & SET HYPERPARAMS
-parser = argparse.ArgumentParser()
-parser.add_argument('--seed', help='seed', type=int, default=0)
-args = parser.parse_args()
-logging.info(args)
+
+if __name__ == "__main__":
+  # GET ARGS & SET HYPERPARAMS
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--seed', help='seed', type=int, default=0)
+  args = parser.parse_args()
+  logging.info(args)
 
 
-mode = 'agent'
-#mode = 'human'
-visualize = False
-total_sessions = 200
-t_max = 100
+  mode = 'agent'
+  #mode = 'human'
+  visualize = False
+  total_sessions = 200
+  t_max = 100
 
-# INIT ENVIRONMENT
-env = Env_2048(mode=mode)
-env = EnvDummy(mode=mode)
+  # INIT ENVIRONMENT
+  env = Env_2048(mode=mode)
+  env = EnvDummy(mode=mode)
 
-# INIT POLICY
-policy_random = RandomPolicy(env, verbose=1)
-policy = baselineREINFORCEpolicy(env, model_type='ConvNet', t_max=t_max, verbose=1)
-policy = baselineREINFORCEpolicy(env, model_type='MLP', t_max=t_max, verbose=1)
+  # INIT POLICY
+  policy_random = RandomPolicy(env, verbose=1)
+  policy = baselineREINFORCEpolicy(env, model_type='ConvNet', t_max=t_max, verbose=1)
+  policy = baselineREINFORCEpolicy(env, model_type='MLP', t_max=t_max, verbose=1)
 
-# TRAIN POLICY
-rewards = policy.train(total_sessions=total_sessions)
-rewards_baseline = policy_random.run_sessions(total_sessions, t_max)
+  # TRAIN POLICY
+  rewards = policy.train(total_sessions=total_sessions)
+  rewards_baseline = policy_random.run_sessions(total_sessions, t_max)
 
-# PLOT RESULTS
-plt.plot(rewards, 'ConvNet')
-plt.plot(rewards_baseline, label='baseline')
-plt.legend()
+  # PLOT RESULTS
+  plt.plot(rewards, 'ConvNet')
+  plt.plot(rewards_baseline, label='baseline')
+  plt.legend()
 
-if visualize:
-  grid_visualization = GridVisualization(env=env, model=policy, sleep_time=0.1)
-  #grid_visualization = GridVisualization(env=env, model=policy_random, sleep_time=0.1)
+  if visualize:
+    grid_visualization = GridVisualization(env=env, model=policy, sleep_time=0.1)
+    #grid_visualization = GridVisualization(env=env, model=policy_random, sleep_time=0.1)
