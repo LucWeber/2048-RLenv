7b30508863929f6247be6af70b83b8979afdf1b2
LucWeber <lucasweber000@gmail.com>
2024-04-14 10:35:07 +0200

    Add sampling options

--- RLenv_2048/policies.py
+++ RLenv_2048/policies.py
@@ -40,7 +40,7 @@ class baselineREINFORCEpolicy:
     adapt such that you cannot see where it is from
     """
 
-    def __init__(self, env, model_name='MLP', t_max=1000, verbose=0, gamma=0.99, epsilon=0.0, entropy_term=0.1, lr=1e-4, save_name='model', **kwargs):
+    def __init__(self, env, model_name='MLP', t_max=1000, verbose=0, gamma=0.99, epsilon=0.0, entropy_term=0.1, lr=1e-4, save_name='model', sampling='soft', **kwargs):
         
         input_size = env.observation_space.shape[0] * env.observation_space.shape[1]
 
@@ -48,7 +48,7 @@ class baselineREINFORCEpolicy:
         self.lr = lr
         self.save_name = save_name
         
-        self.model, self.model_type = get_model(model_name=model_name, input_size=input_size, num_actions=env.action_space.n,lr=lr, device=device)
+        self.model, self.model_type = get_model(model_name=model_name, input_size=input_size, num_actions=env.action_space.n,lr=lr, sampling=sampling, device=device)
         self.model = self.model.to(device)
 
         self.verbose = verbose
@@ -63,7 +63,7 @@ class baselineREINFORCEpolicy:
         """ train policy network for given amount of sessions """
         final_scores = []
         self.training_steps = total_sessions
-        current_best_score = 4000 # we want to save models that perform better than this across recent sessions
+        current_best_score = 3000 # we want to save models that perform better than this across recent sessions
         
         pbar = tqdm(range(total_sessions), desc="Train policy")
         for i, session in enumerate(pbar):
@@ -78,11 +78,11 @@ class baselineREINFORCEpolicy:
                                                                t_max=self.t_max, epsilon=self.epsilon)
 
         if save_high_reward_sessions:
-            if self.env.total_score > 7000:
-                save_folder = f'./pretrain_data'
+            if self.env.total_score > 8000:
+                save_folder = f'./RLenv_2048/pretrain_data'
                 os.makedirs(save_folder, exist_ok=True)
                 save_file = f'state_action_pairs_high_reward_{self.env.total_score}_{self.save_name}.pt'
-                torch.save((torch.tensor(states), torch.stack(actions), torch.tensor(rewards)), open(os.path.join(save_folder, save_file), 'wb'))
+                torch.save((torch.tensor(states).cpu(), torch.stack(actions).cpu(), torch.tensor(rewards).cpu()), open(os.path.join(save_folder, save_file), 'wb'))
                 print(f'Saved state-action pairs!')
 
         rewards = torch.tensor(rewards).unsqueeze(dim=0)
@@ -106,13 +106,13 @@ class baselineREINFORCEpolicy:
             for i, session in enumerate(pbar):
                 states, actions, rewards, _ = generate_session(env=self.env, model=self.model,
                                                                     t_max=t_max, epsilon=self.epsilon)
-
+                self.print_distribution_actions(actions)
                 if save_high_reward_sessions:
-                    if self.env.total_score > 7000:
-                        save_folder = f'./pretrain_data'
+                    if self.env.total_score > 8000:
+                        save_folder = f'./RLenv_2048/pretrain_data'
                         os.makedirs(save_folder, exist_ok=True)
                         save_file = f'state_action_pairs_high_reward_{self.env.total_score}_{self.save_name}.pt'
-                        torch.save((torch.tensor(states), torch.stack(actions), torch.tensor(rewards)), open(os.path.join(save_folder, save_file), 'wb'))
+                        torch.save((torch.tensor(states).cpu(), torch.stack(actions).cpu(), torch.tensor(rewards).cpu()), open(os.path.join(save_folder, save_file), 'wb'))
                         print(f'Saved state-action pairs!')
                 pbar.set_postfix(reward=f'{self.env.total_score}', refresh=False)
 
@@ -131,6 +131,7 @@ class baselineREINFORCEpolicy:
     def update_policy(self, batch_rewards, batch_log_probs):
         policy_gradients = []
 
+        entropy = 0
         for rewards, log_probs in zip(batch_rewards, batch_log_probs):
             discounted_rewards = get_cumulative_rewards(rewards, self.gamma)
             discounted_rewards = torch.tensor(discounted_rewards)
@@ -142,20 +143,20 @@ class baselineREINFORCEpolicy:
             
             # Calculate entropy of the action probabilities
             probs = torch.exp(log_probs)
-            entropy = -(probs * log_probs).sum(-1).mean()
-            regularization_term = -(self.entropy_term * entropy)
+            entropy += -(probs * log_probs).sum(-1).mean()
+            #regularization_term = -(self.entropy_term * entropy)
+
+        entropy /= len(batch_rewards)
 
         self.model.optimizer.zero_grad()
 
-        policy_gradient = torch.stack(policy_gradients).sum() + regularization_term
-
+        policy_gradient = (1 - self.entropy_term) * torch.stack(policy_gradients).sum() - (self.entropy_term * entropy)
         policy_gradient.backward()
-
         self.model.optimizer.step()
 
 
     def save_model(self, best=False, verbose=True):
-        save_folder = f'./saved_models'
+        save_folder = f'./RLenv_2048/saved_models'
         os.makedirs(save_folder, exist_ok=True)
 
         save_file = f'REINFORCE_{self.save_name}{"_best" if best else ""}.ckpt'
@@ -181,14 +182,14 @@ def generate_session(env, model, t_max, epsilon=0.0):
     state = env.reset()
 
     for _ in range(t_max):
-
         # this is for faking a batch:
         inputs = torch.tensor(state, dtype=torch.float32).unsqueeze(dim=0).to(device)
         inputs.requires_grad = True
-        action, log_prob = model.get_action(inputs, epsilon=epsilon)
+        legal_actions = env.get_legal_actions()
+        action, log_prob = model.get_action(inputs, legal_actions, epsilon=epsilon)
         log_probs.append(log_prob)
 
-        # Sample action with given probabilities.
+        # take action
         new_state, reward, done, info = env.step(int(action))
 
         # record session history to train later
@@ -199,7 +200,7 @@ def generate_session(env, model, t_max, epsilon=0.0):
         state = new_state
         if done:
             break
-
+    
     return states, actions, rewards, log_probs
 
 
